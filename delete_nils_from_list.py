5de6fc051c8bb5cb5178f1980270d55e36a4cfad
57BBBears <seocreomail@gmail.com>
2024-05-01 00:19:34 +0300

    delete_nils_from_list

---
+++ delete_nils_from_list.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def delete_nils(list_: list[int]):
+    """
+    Without list comprehension
+    :param list_: a list of numbers
+    :return: a list without nils
+    """
+    pivot = 0
+    i = 0
+    while i < len(list_):
+        if list_[i] == 0:
+            list_.pop(i)
+            list_.insert(pivot, 0)
+            pivot += 1
+
+        i += 1
+
+    return list_[pivot:]
+
+
+if __name__ == "__main__":
+    numbers = [int(sys.argv[i]) for i in range(1, len(sys.argv))]
+    print(delete_nils(numbers))
