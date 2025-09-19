15f180ae81fce5986374f0d21ba045143dc88d7d
57BBBears <seocreomail@gmail.com>
2025-09-19 12:33:00 +0300

    fix: two sum

--- leetcode/easy/two_sum.py
+++ leetcode/easy/two_sum.py
@@ -18,5 +18,25 @@ def test_task():
     assert get_sum_indeces([3, 2, 4], 6) == [1, 2]
 
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mb_nums = {}
+
+        for i in range(len(nums)):
+            if nums[i] in mb_nums:
+                return [mb_nums[nums[i]], i]
+
+            mb_nums[target - nums[i]] = i
+
+
+def test_solution():
+    s = Solution()
+
+    assert s.twoSum([3, 3], 6) == [0, 1]
+    assert s.twoSum([2, 7, 11, 15], 9) == [0, 1]
+    assert s.twoSum([3, 2, 4], 6) == [1, 2]
+
+
 if __name__ == "__main__":
     test_task()
+    test_solution()
